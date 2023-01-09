c4f6bab4368b14f38d932b034b45906fc8a3d72b
Ajeeth050699 <121790206+Ajeeth050699@users.noreply.github.com>
2023-01-09 16:35:34 +0530

    design of varal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,476 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import title from "./Assets/Group 114.png";
+import circle1 from "./Assets/Ellipse 16.png";
+import circle2 from "./Assets/Ellipse 20.png";
+import circle3 from "./Assets/Ellipse 21.png";
+import circle4 from "./Assets/Ellipse 19-1.png";
+import img1 from "./Assets/ILLUSTRATION.png";
+import SemiSide from "./Assets/Group 100.png";
+import circle5 from "./Assets/Ellipse 23.png";
+import vector1 from "./Assets/Vector-1.png";
+import vector2 from "./Assets/Vector-2.png";
+import circle6 from "./Assets/Ellipse 19.png";
+import circle7 from "./Assets/Ellipse 24.png";
+import circle8 from "./Assets/Ellipse 12.png";
+import circle9 from "./Assets/Ellipse 25.png";
+import circle10 from "./Assets/Ellipse 15.png";
+import circle11 from "./Assets/Ellipse 7.png";
+import circle12 from "./Assets/Ellipse 14.png";
+import circle13 from "./Assets/Ellipse 19-1.png";
+import img2 from "./Assets/ILLUSTRATION 03  1.png";
+import img3 from "./Assets/Group-2.png";
+import img4 from "./Assets/Group.png";
+import img5 from "./Assets/Group-1.png";
+import img6 from "./Assets/dungeon.png";
+import img7 from "./Assets/Group 35.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-bar">
+        <img src={title} className="title-img"></img>
+
+        <div className="nav-title">
+          <a href="#" className="title1">
+            Home
+          </a>
+          <a href="#" className="title">
+            Services
+          </a>
+          <a href="#" className="title">
+            Pricing
+          </a>
+          <a href="#" className="title">
+            About us
+          </a>
+          <button className="title-btn">Start a Company</button>
+        </div>
+      </div>
+      <div className="two-line">
+        <div className="circle-gap1">
+          <img src={circle1} className="circle1" />
+        </div>
+        <div className="circle-gap2">
+          <img src={circle2} className="circle2" />
+        </div>
+        <div className="search-input">
+          <form className="nosubmit">
+            <input
+              className="nosubmit"
+              type="search"
+              placeholder="Search a Term | Topic"
+            />
+          </form>
+        </div>
+      </div>
+      <div className="third-line">
+        <div className="circle-gap3">
+          <img src={circle3} className="circle1" />
+        </div>
+        <div className="circle-gap4">
+          <img src={circle4} className="circle2" />
+        </div>
+      </div>
+      <div className="fourth-line">
+        <div>
+          <div className="heading">Claim a Free Quote</div>
+          <div className="heading-1">
+            <div className="started">Get started</div>
+            <div className="started1">on fullfilling</div>
+          </div>
+          <div className="started1">your Dubai or UAE</div>
+          <div className="started1">dream.</div>
+          <div className="heading1">UAE & Offshore Company</div>
+          <div className="started2">
+            We provide you with information about UAE or
+          </div>
+          <div className="started3">
+            Offshore Company Registration & help you
+          </div>
+          <div className="started3">
+            setup your company with a bank account and
+          </div>
+          <div className="started3">visas.</div>
+          <div class="topnav">
+            <a class="active" href="#home">
+              Start a Company
+            </a>
+            <a href="#news">Renew a Company</a>
+          </div>
+        </div>
+        <div>
+          <img src={img1} className="images"></img>
+        </div>
+      </div>
+      <div className="fifth">
+        <div className="fifth-line">
+          Watch the video about UAE or Offshore Company Registration
+        </div>
+        <div className="box-circle1" />
+      </div>
+      <div className="sixth-line">
+        <div>
+          <img src={SemiSide} className="semicircle"></img>
+        </div>
+        <div>
+          <img src={circle5} className="circle-5"></img>
+        </div>
+        <div className="texted-lines">
+          <div className="text-1">Dedicated</div>
+          <div className="text-2">to our mission we are</div>
+          <div className="text-3">
+            Our services include Company Formation & Renewals,
+          </div>
+          <div className="text-4">
+            Trust & Fiduciary, Tax Residency Setup With Family, Bank
+          </div>
+          <div className="text-4">
+            Accounts, Remote Management, Stock Trading
+          </div>
+          <div className="text-4">Platforms, Ownership Solutions.</div>
+        </div>
+      </div>
+      <div class="grid-container">
+        <div className="one-coloumn">
+          <img src={img6} className="" />
+          <div className="head1">UAE Free Zone Company</div>
+          <div className="head2">
+            Your registration agent, will answer all of your questions and help
+            you to reach a conclusion that meets your objectives of investing in
+            the UAE
+          </div>
+          <a href="#" className="get-start">
+            Get Started
+          </a>
+        </div>
+        <div>
+          <img src={vector2} className="" />
+          <div className="head1">Dubai Local Companies</div>
+          <div className="head2">
+            The Dubai LLC formation documents are actually articles of
+            organization or a certificate of organization. You can get yours
+            today.
+          </div>
+          <a href="#" className="get-start">
+            Get Started
+          </a>
+        </div>
+        <div>
+          <img src={vector1} className="" />
+          <div className="heads2">Offshore Companies</div>
+          <div className="head2">
+            You can register an offshore company and open its bank account in
+            Dubai. Your agent will help you along all the process
+          </div>
+          <a href="#" className="get-start">
+            Get Started
+          </a>
+        </div>
+      </div>
+      <div className="seventh-line">
+        <div className="circle6">
+          <img src={circle6} className="" />
+        </div>
+        <div className="circle7">
+          <img src={circle7} className="" />
+        </div>
+      </div>
+      <div className="grid-container3">
+        <div className="coloumn-1">
+          <div className="line-1">
+            Learn the ways in which you can benefit from a UAE/Offshore Company.
+            Then get started on fulfilling your UAE dream.
+          </div>
+          <div className="line-2">Claim a Free Quote</div>
+          <div className="box-1">
+            <div className="imgess">
+              <img src={img2} className="img2"></img>
+            </div>
+            <div className="line-3">Bank Account Setup </div>
+            <div className="line-4">
+              There are many banks in the United Arab Emirates [UAE]. Both
+              locally owned and branches of multinational ones. Foreign banks
+              adjust according to their parent’s strategies and have changed
+              local requirements overnight in the past. But we are here to help
+              you.
+            </div>
+            <a href="#" className="learn">
+              Learn More ->
+            </a>
+          </div>
+          <img src={circle8} className="circles"></img>
+          <div className="box-2">
+            <div className="imgess">
+              <img src={img3} className="img2"></img>
+            </div>
+            <div className="line-3">UAE Company Visas </div>
+            <div className="line-4">
+              There are many banks in the United Arab Emirates [UAE]. Both
+              locally owned and branches of multinational ones. Foreign banks
+              adjust according to their parent’s strategies and have changed
+              local requirements overnight in the past. But we are here to help
+              you.
+            </div>
+            <a href="#" className="learn">
+              Learn More ->
+            </a>
+          </div>
+        </div>
+        <div className="coloumn-2">
+          <div className="box-3">
+            <div className="imgess">
+              <img src={img4} className="img3"></img>
+            </div>
+            <div className="line-3">Advice & Guidance </div>
+            <div className="line-4">
+              All activities in the UAE are licensed. Whether manufacturing,
+              finance, trading, marketing, consultancy or restaurants. In some
+              countries only manufacturing is licensed. In others there is a
+              threshold below which business are encouraged. Get our insightfull
+              guidance today.
+            </div>
+            <a href="#" className="learn">
+              Learn More ->
+            </a>
+          </div>
+          <div className="circle-line">
+            <div className="circle9">
+              <img src={circle9} className=""></img>
+            </div>
+            <div>
+              <img src={circle10} className=""></img>
+            </div>
+          </div>
+          <div className="box-4">
+            <div className="imgess">
+              <img src={img5} className="img4"></img>
+            </div>
+            <div className="line-3">Registration Document Perparation</div>
+            <div className="line-4">
+              Several documents must be prepared to start the process of
+              registering a new company in the United Arab Emirates. Be it a
+              Dubai local company, a free zone one or an offshore entity. Your
+              registered agent is dedicated to get this done for you for a
+              seamless process.
+            </div>
+            <a href="#" className="learn">
+              Learn More ->
+            </a>
+          </div>
+          <div className="describe">
+            We have gathered a team of professionals to craft adequate services
+            you can rely on for a friction free setup in UAE.
+          </div>
+          <a href="#" className="services">
+            More about our services ->
+          </a>
+        </div>
+      </div>
+      <div className=" eight-line">
+        <div>
+          <img src={circle11} className=""></img>
+        </div>
+        <div>
+          <img src={circle12} className=""></img>
+        </div>
+      </div>
+
+      <div className="ninth-line">Most popular affordable pricing per</div>
+      <div className="lines"></div>
+      <div className="ninth-line"> jurisdiction are bought you to</div>
+      <div className="ninth-line"> kick off you adventure</div>
+      <img src={circle13} className="circle13"></img>
+      <div className="grid-container4">
+        <div className="box-5">
+          <div className="name">AJMAN OFFSHORE</div>
+          <div className="price">
+            <div className="pricing">$2,997</div>
+            <div className="price-color">One Time Payment</div>
+          </div>
+          <div className="role-describe">
+            Ajman Offshore New Company formation includes
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Attestation Charges</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Registered Agent</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Registered Office</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Varal Administrative</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Preparing Statutory File</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">
+              Liasing with Registration Authorities
+            </div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Due Diligence Appraisa</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Keeping The Register</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Seal Charges</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Certificate of Good Standing</div>
+          </div>
+          <button className="one-button">Continue</button>
+        </div>
+        <div className="box-5">
+          <div className="name">RANK ICC</div>
+          <div className="price">
+            <div className="pricing">$2,997</div>
+            <div className="price-color">One Time Payment</div>
+          </div>
+          <div className="role-describe">
+            Rak Icc New Company formation includes
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Attestation Charges</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Registered Agent</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Registered Office</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Varal Administrative</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Preparing Statutory File</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">
+              Liasing with Registration Authorities
+            </div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Due Diligence Appraisa</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Keeping The Register</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Seal Charges</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Certificate of Good Standing</div>
+          </div>
+          <button className="one-button">Continue</button>
+        </div>
+        <div className="box-5">
+          <div className="name">SHARJAH MEDIA CITY</div>
+          <div className="price">
+            <div className="pricing">$2,997</div>
+            <div className="price-color">One Time Payment</div>
+          </div>
+          <div className="role-describe">
+            Sharjah Media City New Company formation include
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Attestation Charges</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Registered Agent</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Registered Office</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Varal Administrative</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Preparing Statutory File</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">
+              Liasing with Registration Authorities
+            </div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Due Diligence Appraisa</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Keeping The Register</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Seal Charges</div>
+          </div>
+          <div className="one">
+            <img src={img7} alt="tick" className="tick-image"></img>
+
+            <div className="tick-describe">Certificate of Good Standing</div>
+          </div>
+          <button className="one-button">Continue</button>
+        </div>
+      </div>
     </div>
   );
 }
